69096bfbfb006e20ba7066dbffc0164f1d2e42d4
Jafari3059 <fereshtejafari3059@gmail.com>
2024-01-08 12:58:40 +0330

    add empty and full hours to class time

--- finalProject.cpp
+++ finalProject.cpp
@@ -6,49 +6,45 @@ using namespace std;
 const int MAX_COURSES = 100;
 
 enum Day {
-      SUNDAY,
-      MONDAY,
-      TUESDAY,
-      WEDNESDAY,
-      THURSDAY,
-      FRIDAY,
-      SATURDAY
+    SUNDAY,
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY
 };
 
 class Time {
-      int startingHour;
-      int startingMinute;
-      int hourDuration;
-      int minuteDuration;
-      
-      public: Time() {
-            startingHour = 0;
-            startingMinute = 0;
-            hourDuration = 0;
-            minuteDuration = 0;
-      }
-      
-      public: Time(int startingHour, int startingMinute, int hourDuration, int minuteDuration)
-      {
-            this->startingHour = startingHour;
-            this->startingMinute = startingMinute;
-            this->hourDuration = hourDuration;
-            this->minuteDuration = minuteDuration;
-      }
+    int startingHour;
+    int startingMinute;
+    int hourDuration;
+    int minuteDuration;
+    
+    int fullHours[9];
+    int emptyHours[9];
+
+    public: Time(int startingHour = 0, int startingMinute = 0, int hourDuration = 0, int minuteDuration = 0)
+    {
+        this->startingHour = startingHour;
+        this->startingMinute = startingMinute;
+        this->hourDuration = hourDuration;
+        this->minuteDuration = minuteDuration;
+    }
 };
 
 class ClassLocation {
-      int static counter;
-      int classNumber;
-      
-      public: ClassLocation()
-      {
-            this->classNumber = counter++;
-      }
+    int static counter;
+    int classNumber;
+
+    public: ClassLocation()
+    {
+        this->classNumber = counter++;
+    }
 };
 
 class Course {
-      static int counter;
+    static int counter;
     int id;
     string name;
     string teacher;
@@ -61,16 +57,16 @@ class Course {
    
     Course(string name, string teacher, Day day, Time time, bool hasProjector, int studentCapacity, ClassLocation location)
     {
-      this->id = counter++;
-      this->name = name;
-      this->teacher = teacher;
-      this->day = day;
-      this->time = time;
-      this->hasProjector = hasProjector;
-      this->studentCapacity = studentCapacity;
-      this->classLocation = classLocation;
-      int* studentList = new int[studentCapacity];
-      }
+        this->id = counter++;
+        this->name = name;
+        this->teacher = teacher;
+        this->day = day;
+        this->time = time;
+        this->hasProjector = hasProjector;
+        this->studentCapacity = studentCapacity;
+        this->classLocation = classLocation;
+        int* studentList = new int[studentCapacity];
+    }
 };
 
 int ClassLocation::counter = 0;
